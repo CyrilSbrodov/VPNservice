9e0b586ab92a8621308cf1ae12fa38fcb4733cbb
CyrilSbrodov <sbrandy@bk.ru>
2022-09-02 15:20:41 +0300

    new functions

--- internal/vpndata/vpn.go
+++ internal/vpndata/vpn.go
@@ -4,27 +4,58 @@ import (
 	"bytes"
 	"fmt"
 	"golang.org/x/text/encoding/charmap"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"os/exec"
+	"sort"
 	"strconv"
 	"strings"
+	"sync"
 )
+
+type VPN struct {
+	Name    string
+	IP      string
+	Country string
+	Ping    int
+}
 
 func GetVPN() {
 
+	filename := "internal/vpndata/vpn.csv"
+
+	req, err := http.Get("http://www.vpngate.net/api/iphone/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer req.Body.Close()
+	b, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	file, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	ioutil.WriteFile(filename, b, 0644)
+
 }
 
-func CreateVPNConnection() {
-	//site := "https://www.vpngate.net/en/"
-	vpnName := "Add-VpnConnection -Name \"123\" -ServerAddress \"219.100.37.60\" -TunnelType L2TP -L2tpPsk \"vpn\" -Force -AuthenticationMethod MSChapv2 -RememberCredential"
+func CreateVPNConnection(vpn []VPN) {
+	sort.SliceStable(vpn, func(i, j int) bool {
+		return vpn[i].Ping < vpn[j].Ping
+	})
+	fmt.Println(vpn)
 
+	vpnName := fmt.Sprintf("Add-VpnConnection -Name \"%v\" -ServerAddress \"%v\" -TunnelType Automatic -L2tpPsk \"vpn\" -Force -AuthenticationMethod MSChapv2 -RememberCredential", vpn[0].Name, vpn[0].IP)
+	fmt.Println(vpnName)
 	args := strings.Split(vpnName, " ")
 	cmd := exec.Command("powershell.exe", args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-
-	cmd.Run()
 
 	err := cmd.Run()
 
@@ -32,17 +63,15 @@ func CreateVPNConnection() {
 		fmt.Printf("cmd.Run: %s failed: %s\n", err, err)
 	}
 
-	startVPNConnection()
+	startVPNConnection(&vpn[0])
 }
 
-func startVPNConnection() {
-	runVpn := "rasdial \"123\" \"vpn\" \"vpn\""
+func startVPNConnection(vpn *VPN) {
+	runVpn := fmt.Sprintf("rasdial \"%v\" \"vpn\" \"vpn\"", vpn.Name)
 	args := strings.Split(runVpn, " ")
 	cmd := exec.Command("powershell.exe", args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-
-	cmd.Run()
 
 	err := cmd.Run()
 
@@ -57,8 +86,6 @@ func DisconnectVPNConnection() {
 	cmd := exec.Command("powershell.exe", args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-
-	cmd.Run()
 
 	err := cmd.Run()
 
@@ -82,8 +109,6 @@ func RemoveVPNConnection() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	cmd.Run()
-
 	err = cmd.Run()
 
 	if err != nil {
@@ -91,15 +116,15 @@ func RemoveVPNConnection() {
 	}
 }
 
-func Ping() {
+func Ping(vpn *VPN, wg *sync.WaitGroup) {
 
-	args := strings.Split("/c ping google.ru", " ")
+	ip := "/c" + " ping" + " " + vpn.IP
+	args := strings.Split(ip, " ")
 	cmd := exec.Command("powershell.exe", args...)
 
 	var b bytes.Buffer
 	cmd.Stdout = &b
 	cmd.Stderr = os.Stderr
-	cmd.Run()
 
 	err := cmd.Run()
 
@@ -111,19 +136,65 @@ func Ping() {
 	reader, err := dec.Bytes(b.Bytes())
 	str := string(reader)
 	st := strings.Split(str, "\n")
-	if len(st) < 5 {
-		fmt.Println("недоступен")
+	dnd := strings.Contains(str, "Превышен")
+	if len(st) < 4 {
+		vpn.Ping = 9999
+	} else if dnd {
+		vpn.Ping = 9999
 	} else {
+		if len(st) < 12 {
+			wg.Done()
+			return
+		}
 		st1 := strings.Split(st[11], ",")
 		st2 := strings.Split(st1[2], " ")
-		var timePing int
 		for i := 0; i < len(st2); i++ {
 			num, err := strconv.Atoi(st2[i])
 			if err != nil {
 				continue
 			}
-			timePing = num
+			vpn.Ping = num
+			wg.Done()
+			return
 		}
-		fmt.Println("Средний пинг:", timePing)
 	}
+	wg.Done()
+}
+
+func GetVPNlist() []VPN {
+	filename := "internal/vpndata/vpnlist.csv"
+	f, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	file, err := ioutil.ReadFile("internal/vpndata/vpn.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	filelines := strings.Split(string(file), "\n")
+
+	var list VPN
+	var vpnlist []VPN
+
+	for i := 2; i < len(filelines); i++ {
+		vpnstr := ""
+		ip := strings.Split(filelines[i], ",")
+		if len(ip) > 5 {
+			vpnstr = ip[0] + "," + ip[1] + "," + ip[5] + "," + "\n"
+			if _, err := f.WriteString(vpnstr); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+	for i := 2; i < len(filelines); i++ {
+		ip := strings.Split(filelines[i], ",")
+		if len(ip) > 5 {
+			list.Name = ip[0]
+			list.IP = ip[1]
+			list.Country = ip[5]
+			vpnlist = append(vpnlist, list)
+		}
+	}
+	return vpnlist
 }
--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
-import "VPNservice/internal/vpndata"
+import (
+	"VPNservice/internal/vpndata"
+	"fmt"
+	"sync"
+)
 
 func main() {
-	//vpndata.Ping()
-	vpndata.CreateVPNConnection()
-	vpndata.DisconnectVPNConnection()
-	vpndata.RemoveVPNConnection()
+	wg := sync.WaitGroup{}
+
+	//vpndata.DisconnectVPNConnection()
+	//vpndata.RemoveVPNConnection()
+	vpndata.GetVPN()
+
+	vpn := vpndata.GetVPNlist()
+
+	for i := 0; i < len(vpn); i++ {
+		wg.Add(1)
+		go vpndata.Ping(&vpn[i], &wg)
+	}
+	fmt.Println("ждем все горутины")
+	wg.Wait()
+	vpndata.CreateVPNConnection(vpn)
 }
